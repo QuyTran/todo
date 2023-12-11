45b39f7bef316e105370bb3904bb55dc12d2aee4
Quy Tran <tbquy115@gmail.com>
2023-12-11 16:19:57 +0700

    changed

--- api/graphql/resolver.go
+++ api/graphql/resolver.go
@@ -1,22 +1,18 @@
 package graphql
 
 import (
-	gen "todo/api/graphql/generated"
+	"todo/api/graphql/generated"
 	"todo/repositories/ent/ent"
 
 	"github.com/99designs/gqlgen/graphql"
 )
-
-// This file will not be regenerated automatically.
-//
-// It serves as dependency injection for your app, add any dependencies you require here.
 
 // Resolver is the resolver root.
 type Resolver struct{ client *ent.Client }
 
 // NewSchema creates a graphql executable schema.
 func NewSchema(client *ent.Client) graphql.ExecutableSchema {
-	return gen.NewExecutableSchema(gen.Config{
+	return generated.NewExecutableSchema(generated.Config{
 		Resolvers: &Resolver{client},
 	})
 }
--- api/graphql/todo.resolvers.go
+++ api/graphql/todo.resolvers.go
@@ -6,17 +6,21 @@ package graphql
 
 import (
 	"context"
-	"fmt"
 	"todo/api/graphql/generated"
 	"todo/repositories/ent/ent"
 )
 
 // CreateTodo is the resolver for the createTodo field.
 func (r *mutationResolver) CreateTodo(ctx context.Context, input ent.CreateTodoInput) (*ent.Todo, error) {
-	panic(fmt.Errorf("not implemented: CreateTodo - createTodo"))
+	return ent.FromContext(ctx).Todo.Create().SetInput(input).Save(ctx)
 }
 
-// Mutation returns generated.MutationResolver implementation.
+// UpdateTodo is the resolver for the updateTodo field.
+func (r *mutationResolver) UpdateTodo(ctx context.Context, id int, input ent.UpdateTodoInput) (*ent.Todo, error) {
+	return ent.FromContext(ctx).Todo.UpdateOneID(id).SetInput(input).Save(ctx)
+}
+
+// Mutation returns MutationResolver implementation.
 func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
 
 type mutationResolver struct{ *Resolver }
